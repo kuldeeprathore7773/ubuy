dfbff8958543c37cfbef22376244d0e5f21e00d5
Kuldeep Rathore <kuldeeprathore7449@gmail.com>
2024-09-05 22:56:31 +0530

    final submit

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -8,21 +8,46 @@ function Hero() {
         <div className="container">
           <div className="hero-inner">
             <div className="hero-text">
-              <h1>
-                <img src={heroTitle} alt="" srcset="" />
-              </h1>
+              <div className="text-container">
+                <h1 className="wave-text transparent">
+                  <span>M</span>
+                  <span>A</span>
+                  <span>D</span>
+                  <span>E</span>
+                  <span className="space"> </span>
+                  <span>F</span>
+                  <span>O</span>
+                  <span>R</span>
+                </h1>
+
+                <h1 className="wave-text">
+                  <span>F</span>
+                  <span>O</span>
+                  <span>U</span>
+                  <span>N</span>
+                  <span>D</span>
+                  <span>E</span>
+                  <span>R</span>
+                  <span>S</span>
+                </h1>
+
+              </div>
               <div className="services">
                 <div className="service">
                   <h3>Business Capital</h3>
                   <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Quis ipsum suspendisse ultrices gravida.
                   </p>
-                  <button className="btn-dark" >Sign Up</button>
+                  <button className="btn-dark">Sign Up</button>
                 </div>
                 <div className="service">
                   <h3>Mentorship</h3>
                   <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Quis ipsum suspendisse ultrices gravida.
                   </p>
                 </div>
               </div>
@@ -30,7 +55,10 @@ function Hero() {
             <div className="hero-image">
               <img src={heroImg} />
             </div>
-            <p className="hero-banner-rotate-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+            <p className="hero-banner-rotate-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna.
+            </p>
           </div>
         </div>
       </section>
--- src/sections/latest-news/LatestNews.jsx
+++ src/sections/latest-news/LatestNews.jsx
@@ -7,26 +7,48 @@ function LatestNews() {
   return (
     <section className="latest-news">
       <div className="container">
+        <div className="text-container latest-news-text-wave">
+          <h2 className="wave-text transparent">
+            <span>L</span>
+            <span>a</span>
+            <span>t</span>
+            <span>e</span>
+            <span>s</span>
+            <span>t</span>
+            <span className="space"> </span>
+            <span>N</span>
+            <span>e</span>
+            <span>w</span>
+            <span>s</span>
+          </h2>
+        </div>
 
-        <h2> <img src={latestNews} alt="" srcset="" /> </h2>
-       
         <div className="news-items">
           <div className="news-item">
             <img className="news-blog-img" src={newsImg1} alt="News 1" />
             <h3>News Update 01/02/21</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna.
+            </p>
             <button className="btn-dark">Read Now</button>
           </div>
           <div className="news-item">
             <img className="news-blog-img" src={newsImg2} alt="News 2" />
             <h3>News Update 01/02/21</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magn.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magn.
+            </p>
             <button className="btn-dark">Read Now</button>
           </div>
           <div className="news-item">
             <img className="news-blog-img" src={newsImg3} alt="News 3" />
             <h3>News Update 01/02/21</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna.
+            </p>
             <button className="btn-dark">Read Now</button>
           </div>
         </div>
